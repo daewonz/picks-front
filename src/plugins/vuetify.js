9d29525316950b2323eb6750747dd516a9e0a957
leedaewon <daewonz@uracle.co.kr>
2025-01-31 00:41:57 +0900

    vuetify 설정 수정(defaultSet 삭제)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,23 @@
 import '@mdi/font/css/materialdesignicons.css';
 import 'vuetify/styles';
+import { fa } from 'vuetify/iconsets/fa'; // Font Awesome 아이콘셋
+import { mdi } from 'vuetify/iconsets/mdi'; // Material Design Icons 아이콘셋
 
 import { createVuetify } from 'vuetify';
 
 import * as components from 'vuetify/components';
 
 import * as directives from 'vuetify/directives';
-import '@mdi/font/css/materialdesignicons.css';
 
 export default createVuetify({
 	components,
 	directives,
+	icons: {
+		//defaultSet: '', // 기본 아이콘셋을 Font Awesome으로 설정
+		//aliases, // 아이콘 별칭
+		sets: {
+			fa, // Font Awesome
+			mdi, // Material Design Icons
+		},
+	},
 });
