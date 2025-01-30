7bfca860b12fabb4a6bbd8666c06f0462867ffae
leedaewon <daewonz@uracle.co.kr>
2025-01-31 00:42:10 +0900

    샘플 컴포넌트 width 수정(1200px)

--- src/components/sample/sampleComponents.vue
+++ src/components/sample/sampleComponents.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 <template>
-	<div>안녕하세요 컴포넌트 샘플입니다. 이렇게 붙여서 사용합니다!!</div>
+	<div class="sample">안녕하세요 컴포넌트 샘플입니다. 이렇게 붙여서 사용합니다!!</div>
 </template>
-<style></style>
+<style>
+.sample {
+	width: 1200px;
+}
+</style>
