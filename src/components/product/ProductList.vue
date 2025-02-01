65026dc013b7d3c1aa1d05ff473eed78500f4fbd
leedaewon <daewonz@uracle.co.kr>
2025-02-02 02:01:25 +0900

    물건 리스트 for문 전 작업

--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,25 +1,31 @@
 <script setup>
+import { ref, watch, getCurrentInstance } from 'vue';
 //리스트 값을 변수로 받아서 for문으로 가져오기
+const item = ref({ rating: '3.5' });
+const goDetail = () => {
+	alert('개발중@@');
+};
 </script>
 <template>
 	<section class="productList">
-		<v-card class="mx-auto" max-width="400">
+		<v-card class="mx-auto" width="280" @click="goDetail" :ripple="false">
 			<v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
-				<v-card-title>Top 10 Australian beaches</v-card-title>
+				<v-card-title>사진 타이틀</v-card-title>
 			</v-img>
 
-			<v-card-subtitle class="pt-4"> Number 10 </v-card-subtitle>
+			<v-card-subtitle class="pt-5"> 상품명 </v-card-subtitle>
 
 			<v-card-text>
-				<div>Whitehaven Beach</div>
+				<div>가격이랑</div>
 
-				<div>Whitsunday Island, Whitsunday Islands</div>
+				<div>할인시 얼마나 할인인지</div>
 			</v-card-text>
 
 			<v-card-actions>
-				<v-btn color="orange" text="Share"></v-btn>
+				<!-- <v-btn color="orange" text="찜하기"></v-btn>
 
-				<v-btn color="orange" text="Explore"></v-btn>
+				<v-btn color="orange" text="상세보기"></v-btn> -->
+				<v-rating v-model="item.rating" half-increments :disabled="true"></v-rating>
 			</v-card-actions>
 		</v-card>
 	</section>
