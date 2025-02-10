61061820aaa82c306bde3b2d280f33f0d8cdf489
leedaewon <daewonz@uracle.co.kr>
2025-02-10 16:26:49 +0900

    비교용 이벤트 슬라이드바 ver2 추가 및 TODO LIST 수정

---
+++ src/components/events/EventSlider_ver2.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref, watch, getCurrentInstance } from 'vue';
+//리스트 값을 변수로 받아서 for문으로 가져오기
+const item = ref({ rating: '3.5' });
+const goDetail = i => {
+	/*
+	이동시키는 방법 
+	1. url 값을 그대로 db에 넣어서 바로 href로 보내버리기기
+	2. 매칭되어 있는 상품을 찾아서 상세보기 SQL문에서 where절로 가져오기
+	*/
+	console.log('iii', i);
+
+	alert('개발중@@');
+};
+const slides = ref(['First', 'Second', 'Third', 'Fourth', 'Fifth']);
+const colors = ref(['indigo', 'warning', 'pink darken-2', 'red lighten-1', 'deep-purple accent-4']);
+//마우스 올릴 시 (hover) 슬라이드가 자동으로 넘어가는 것을 방지
+const hover = ref(true);
+const onMouseOver = () => {
+	hover.value = false;
+};
+const onMouseLeave = () => {
+	hover.value = true;
+};
+</script>
+
+<template>
+	<v-carousel
+		height="400"
+		show-arrows="hover"
+		:cycle="hover"
+		hide-delimiter-background
+		@mouseover="onMouseOver"
+		@mouseleave="onMouseLeave"
+	>
+		<v-carousel-item v-for="(slide, i) in slides" :key="i">
+			<v-sheet :color="colors[i]" height="100%" @click="goDetail(i)">
+				<div class="d-flex fill-height justify-center align-center">
+					<div class="text-h2">{{ slide }} Slide</div>
+				</div>
+			</v-sheet>
+		</v-carousel-item>
+	</v-carousel>
+</template>
+<style lang="scss">
+.productList {
+	padding: 10px;
+	display: flex;
+	width: 1200px;
+	flex-wrap: wrap;
+	gap: 20px; /* 항목 간 간격 추가 */
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -9,6 +9,7 @@ import SampleComponents2 from '@/components/sample/sampleComponents2.vue';
 import EventSlider from '@/components/events/EventSlider.vue';
 import Button from '@/components/ui/Button.vue';
 import ProductList from '@/components/product/ProductList.vue';
+import EventSlider_ver2 from '@/components/events/EventSlider_ver2.vue';
 const { proxy } = getCurrentInstance();
 const store = useStore();
 const router = useRouter();
@@ -61,13 +62,28 @@ watch(
 <template>
 	<section>
 		<div>TODO리스트</div>
-		<div>버튼에 아이콘 넣기</div>
-		<div>컴포넌트 상품 리스트, 공지사항, 등등으로 분리</div>
-		<div>장바구니 store에서 전역적으로 관리</div>
 		<div>상품 DB 설계(이름, 가격, 평점, 사진, 할인률, 타이틀, 판매자, 상품코드, 카테고리(FK))</div>
 		<div>유저 DB 설계(아이디, 비밀번호(암호화), 쿠폰, 캐시, 찜 )</div>
 		<div>카테고리 DB 설계 (PK, 카테고리)</div>
 		<div>이벤트 페이지 DB설계(PK, 상품코드(FK), 클릭 시 url??, 사진)</div>
+		<div>관리자 페이지 화면 설계</div>
+		<div>판매자 페이지 화면 설계(상품 판매 등록의 주체를 관리자로 할지, 판매자를 따로 만들지 정해야함)</div>
+		<div>상품 상세보기 페이지 화면 설계</div>
+		<div>로그인, 회원가입 페이지 화면 설계</div>
+		<div>버튼에 아이콘 넣기</div>
+		<div>컴포넌트 상품 리스트, 공지사항, 등등으로 분리</div>
+		<div>장바구니 store에서 전역적으로 관리</div>
+		<div>
+			메뉴 및 카테고리 밑으로 내리고 옆으로 클릭하는 형태로 만들기(<a
+				href="https://vuetifyjs.com/en/components/menus/#location"
+				>링크</a
+			>)
+		</div>
+		<div>
+			인피니티 스크롤 (
+			<a href="https://vuetifyjs.com/en/components/infinite-scroller/#anatomy"> 링크 </a>
+			)
+		</div>
 		<div></div>
 		<v-btn @click="goAlert('이건 알럿입니다.')">알럿 호출</v-btn>
 		<v-btn @click="goConfirm">컨펌 호출</v-btn>
@@ -82,6 +98,7 @@ watch(
 		<Button icon="mdi-cart" @click="goConfirm" type="green" size="small" />
 		<ModalTemplate :modalopen="modalopen" />
 		<EventSlider />
+		<EventSlider_ver2 />
 		<!-- <SampleComponents />
 		<SampleComponents2 /> -->
 		<ProductList />
