4cfa5a2d8a845279dfc31400439d1cf7f7c8a443
leedaewon <daewonz@uracle.co.kr>
2025-02-11 17:26:57 +0900

    Button 수정(외곽선 유무)

--- src/components/ui/Button.vue
+++ src/components/ui/Button.vue
@@ -1,16 +1,29 @@
 <script setup>
 import { ref, watch, getCurrentInstance } from 'vue';
 //(길이 조절, 뭘 쓸건지, 색깔 조절, 비활성화 등등)
-const props = defineProps(['text', 'type', 'size', 'icon', 'disabled', 'marginLeft']);
+const props = defineProps(['text', 'type', 'size', 'icon', 'disabled', 'marginLeft', 'outlined']);
 const emit = defineEmits('click');
 const click = () => {
 	emit('click');
 };
+
+const isOutLine = ref('outlined');
 const loading = ref(false);
+
+//외곽선 넣을지 유뮤. 외곽선이 필요하지 않다면 버튼에 :outlined="false" 넣으면 작동.
+watch(
+	() => props,
+	() => {
+		if (props.outlined === false) {
+			isOutLine.value = 'text';
+		}
+	},
+	{ immediate: true },
+);
 </script>
 <template>
 	<section class="btn-wrap">
-		<v-btn class="btn" variant="outlined" :disabled="props.disabled" :loading="loading" @click.stop="click">
+		<v-btn class="btn" :variant="isOutLine" :disabled="props.disabled" :loading="loading" @click.stop="click">
 			<span v-if="props.icon != '' && props.icon != undefined">
 				<v-icon :icon="props.icon" />
 			</span>
@@ -20,7 +33,7 @@ const loading = ref(false);
 </template>
 <style lang="scss">
 html.dark-mode .btn {
-	background-color: rgb(61, 56, 56);
+	background-color: var(--color-bg);
 }
 .btn-wrap {
 	pointer-events: none; /* 기본적으로 클릭 이벤트 차단 */
