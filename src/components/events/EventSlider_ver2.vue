b11b2b06db5f0007516382375604d293a0380602
leedaewon <daewonz@uracle.co.kr>
2025-02-27 17:08:43 +0900

    이벤트 슬라이더 수정

--- src/components/events/EventSlider_ver2.vue
+++ src/components/events/EventSlider_ver2.vue
@@ -14,6 +14,7 @@ const goDetail = i => {
 };
 const slides = ref(['First', 'Second', 'Third', 'Fourth', 'Fifth']);
 const colors = ref(['indigo', 'warning', 'pink darken-2', 'red lighten-1', 'deep-purple accent-4']);
+const currentSlide = ref(0); // 중앙 슬라이드의 인덱스
 //마우스 올릴 시 (hover) 슬라이드가 자동으로 넘어가는 것을 방지
 const hover = ref(true);
 const onMouseOver = () => {
@@ -25,29 +26,71 @@ const onMouseLeave = () => {
 </script>
 
 <template>
-	<v-carousel
-		height="400"
-		show-arrows="hover"
-		:cycle="hover"
-		hide-delimiter-background
-		@mouseover="onMouseOver"
-		@mouseleave="onMouseLeave"
-	>
-		<v-carousel-item v-for="(slide, i) in slides" :key="i">
-			<v-sheet :color="colors[i]" height="100%" @click="goDetail(i)">
-				<div class="d-flex fill-height justify-center align-center">
-					<div class="text-h2">{{ slide }} Slide</div>
-				</div>
-			</v-sheet>
-		</v-carousel-item>
-	</v-carousel>
+	<div class="slider-flex">
+		<!-- left -->
+		<v-carousel class="eventSlider sideSlider" height="400" :show-arrows="false" :cycle="hover" :hide-delimiters="true">
+			<v-carousel-item v-for="(slide, i) in slides" :key="i">
+				<v-sheet v-if="0 <= currentSlide - 1" :color="colors[currentSlide - 1]" height="100%">
+					<div class="d-flex fill-height justify-center align-center">
+						<div class="text-h2">{{ slide }} Slide ~~ {{ currentSlide - 1 }}</div>
+					</div>
+				</v-sheet>
+				<v-sheet v-else :color="'#9ea3a0'" height="100%">
+					<div class="d-flex fill-height justify-center align-center">
+						<div class="text-h2">NoSlide {{ currentSlide - 1 }}</div>
+					</div>
+				</v-sheet>
+			</v-carousel-item>
+		</v-carousel>
+		<!-- center -->
+		<v-carousel
+			class="eventSlider mainSlider"
+			height="400"
+			show-arrows="hover"
+			:cycle="hover"
+			hide-delimiter-background
+			@mouseover="onMouseOver"
+			@mouseleave="onMouseLeave"
+			v-model="currentSlide"
+		>
+			<v-carousel-item v-for="(slide, i) in slides" :key="i">
+				<v-sheet :color="colors[i]" height="100%" @click="goDetail(i)">
+					<div class="d-flex fill-height justify-center align-center">
+						<div class="text-h2">{{ slide }} Slide ~~ {{ currentSlide }}</div>
+					</div>
+				</v-sheet>
+			</v-carousel-item>
+		</v-carousel>
+		<!-- right -->
+		<v-carousel class="eventSlider sideSlider" height="400" :show-arrows="false" :cycle="hover" :hide-delimiters="true">
+			<v-carousel-item v-for="(slide, i) in slides" :key="i">
+				<v-sheet v-if="currentSlide + 1 < slides.length" :color="colors[currentSlide + 1]" height="100%">
+					<div class="d-flex fill-height justify-center align-center">
+						<div class="text-h2">{{ slide }} Slide ~~ {{ currentSlide + 1 }}</div>
+					</div>
+				</v-sheet>
+				<v-sheet v-else :color="'#9ea3a0'" height="100%">
+					<div class="d-flex fill-height justify-center align-center">
+						<div class="text-h2">NoSlide {{ currentSlide + 1 }}</div>
+					</div>
+				</v-sheet>
+			</v-carousel-item>
+		</v-carousel>
+	</div>
 </template>
 <style lang="scss">
-.productList {
-	padding: 10px;
+.slider-flex {
 	display: flex;
-	width: 1200px;
-	flex-wrap: wrap;
-	gap: 20px; /* 항목 간 간격 추가 */
+}
+.mainSlider {
+	flex: 2;
+}
+.eventSlider {
+	width: 32.5vw;
+}
+.sideSlider {
+	flex: 1;
+	filter: blur(2px);
+	transition: filter 0.3s ease, transform 0.3s ease; /* 애니메이션 추가 */
 }
 </style>
